488d106995d5ea9ccb027a16caa5d670ebbbb028
rafal.zientara <rzientara@blackberry.com>
2019-02-23 18:26:22 +0100

    base demo app

---
+++ food_porn_flutter/android/app/src/main/java/com/apps/sample/zientara/rafal/foodpornflutter/MainActivity.java
@@ -0,0 +1,13 @@
+package com.apps.sample.zientara.rafal.foodpornflutter;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
